40962587468b2215768a9a4523ad9bfaab170593
Henry Lin <4henrylin17@gmail.com>
2023-03-24 14:23:17 -0400

    Add random

--- lib/tree_node.rb
+++ lib/tree_node.rb
@@ -1,3 +1,17 @@
 class PolyTreeNode
-  
+    def initialize(new_node, children = [])
+        @value = new_node
+        @parent = nil
+        @children = children
+    end
+
+    def parent
+        @parent
+    end
+
+    def children
+        @children
+    end
+
+
 end
