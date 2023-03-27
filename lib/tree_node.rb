f3889e30ec4213692d7506ce889c03229c32995a
Henry Lin <4henrylin17@gmail.com>
2023-03-27 11:02:43 -0400

    Finish reassigning

--- lib/tree_node.rb
+++ lib/tree_node.rb
@@ -1,14 +1,37 @@
 class PolyTreeNode
-    attr_accessor :value, :parent, :children
+    # attr_accessor :value, :parent, :children
     def initialize(value)
         @value = value
         @parent = nil
         @children = []
     end
 
-    def parents=(node)
-        @parent = node 
-        node.children << self 
+    def value
+        @value
     end
+    
+    def parent
+        @parent
+    end
+
+    def children
+        @children
+    end
+
+    def parent=(node = nil)
+        if node.nil?
+            @parent = nil
+        elsif @parent != nil
+            old_parent = @parent
+            old_parent.children.delete_if{|ele| ele == self}
+            @parent = node 
+            node.children << self if !node.children.include?(self)
+        else
+            @parent = node 
+            node.children << self if !node.children.include?(self)
+        end
+    end
+
+
 
 end
